c885cd595c7019b14b921d6286c854ba81c7d0e4
Nealon01 <nealonkid@gmail.com>
2016-03-28 23:04:25 -0400

    Just Kidding deadline extended
    All working but cookies and styles

--- template/quiz2.js
+++ template/quiz2.js
@@ -1,27 +1,3 @@
-var items = [];
-
-function request() {
-    $.ajax({
-        method: "GET",
-        url: "http://www.mattbowytz.com/simple_api.json?data=quizData",
-        dataType: "json"
-    })
-	.done(function (msg) {
-	    items = msg.data;
-	    changeIt();
-	    $('#changeIt').show();
-	    $('#keepIt').show();
-	});
-}
-
-function changeIt() {
-    $('#title').text(items[Math.round(Math.random() * items.length)])
-}
-
-function keepIt() {
-    document.cookie = "title=" + $('#title').text();
-}
-
 (function ($) {
 	// This is where you will write your function for the programming challenge
 	// Do not commit console.log statements
@@ -33,27 +9,29 @@ function keepIt() {
 	$submit = $('.submit');
 	$timeout = $('.timeout');
 
-	$mouseover.on('mouseover', function() {
+	$mouseover.on('mouseover', function () {
 		$this = $(this);
 		$(this).html('Scrooge McDuck!');
 		$(this).height($(this).height() + 50);
 	});
 
-	$click.click('click', function() {
+	$click.click('click', function () {
 		$this.html('Peace Out!')
 		$(this).fadeOut(1500);
 		return false;
 	});
 
+	$("form").submit(function (e) {
+	    console.log("submit");
+	    e.preventDefault();
+	    if ($(this).find('input[type="text"]').val() !== '') {
+	        $(this).find("input").fadeOut('slow');
+	        $(this).append("<h2>Congratulations! You've entered some text!</h2>");
+	    }
+	});
+
 	$submit.on('submit', function (e) {
-	    console.log("here");
-		e.preventDefault();
-		if ($(this).find('input[type="text"]').val() !== '') {
-			$(this).find('input').foreach(function() {
-				$(this).fadeout('slow');
-			});
-			$(this).appendwith("<h2>Congratulations! You've entered some text!</h2>");
-		}
+	    
 	});
 
 	$(document).ready(function () {
@@ -62,8 +40,10 @@ function keepIt() {
 	    {
 	        $('#title').text(x.substring(6, x.length));
 	    }
-	    $('#changeIt').hide();
 	    $('#keepIt').hide();
+
+	    $('.timeout').hide();
+
 	    setTimeout(function () {
 	        $('.timeout').fadeIn('slow');
 	    }, 1000);
@@ -71,3 +51,26 @@ function keepIt() {
 
 })(jQuery);
 
+var items = [];
+
+function request() {
+    $.ajax({
+        method: "GET",
+        url: "http://www.mattbowytz.com/simple_api.json?data=quizData",
+        dataType: "json"
+    })
+	.done(function (msg) {
+	    items = msg.data;
+	    $('#title').text(items[Math.round(Math.random() * items.length)])
+        $('#getTitle').val("Change it")
+	    $('#keepIt').show();
+	});
+}
+
+function changeIt() {
+    
+}
+
+function keepIt() {
+    document.cookie = "title=" + $('#title').text();
+}
